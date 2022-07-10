7b391b801d51196868b450003c3ac8d969203d4c
Callum-OP <callumoprey@gmail.com>
2022-07-10 11:31:41 +0100

    Added option to edit notes

--- note-book-vue/public/view-notes.js
+++ note-book-vue/public/view-notes.js
@@ -1,3 +1,23 @@
+const setEdit = (id) => {
+    // Get information about the note using id
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", `http://localhost:3000/note/${id}`, false);
+    xhttp.send();
+
+    const note = JSON.parse(xhttp.responseText);
+
+    const {
+        description
+    } = note;
+
+    // Filling information about the note in the form inside the modal
+    document.getElementById('description').value = description;
+
+    // Setting up the action url for the note
+    document.getElementById('editForm').action = `http://localhost:3000/note/${id}`;
+}
+
 const deleteNote = (id) => {
     const xhttp = new XMLHttpRequest();
 
@@ -27,6 +47,7 @@ const loadNotes = () => {
                         <div>Created: ${note.createdAt}</div>
 
                         <button onClick="deleteNote(${note.id})"> Delete </button>
+                        <button onClick="setEdit(${note.id})" data-target="#edit"> Edit </button>
                     <hr>
                     </div>
                 </div>
